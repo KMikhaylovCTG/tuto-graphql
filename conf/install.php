6e8875a1797d804ebeed4085c40e8ccf30bc877e
Sylvain Conny <sylvain.conny@jalis.fr>
2016-06-23 18:20:51 +0200

    corrections installation

--- conf/install.php
+++ conf/install.php
@@ -1,14 +1,25 @@
 <?php
 if (!IS_INSTALLED) {
+    if (!function_exists('mkdir_safe')) {
+        function mkdir_safe($dirname) {
+            if (!is_dir(dirname($dirname))) {
+                mkdir(dirname($dirname));
+            }
+            if (!is_dir($dirname)) {
+                mkdir($dirname);
+            }
+        }
+    }
     exec('npm install', $installOutput);
     if (!empty($installOutput)) {
         foreach ($installOutput as $line) {
             echo "<li>{$line}</li>";
         }
     }
-    mkdir(REACT_SRC_DIR);
-    mkdir(REACT_LIB_DIR);
-    mkdir(GRAPHQL_DIR);
+    mkdir_safe(REACT_SRC_DIR);
+    mkdir_safe(REACT_LIB_DIR);
+    mkdir_safe(GRAPHQL_DIR);
     copy(DEFAULT_DIR.'/schema.js', SERVER_DIR.'/schema.js');
     echo '<li>Installation terminée.</li>';
 }
+
