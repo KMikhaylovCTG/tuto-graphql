a9ee69d8e6c247cd85442fbac67666083c576d2d
Sylvain Conny <sylvain.conny@jalis.fr>
2016-06-23 18:02:49 +0200

    corrections constantes php

--- conf/path.php
+++ conf/path.php
@@ -1,10 +1,10 @@
 <?php
 
-define(ROOT_DIR, dirname(__DIR__).'/');
+define('ROOT_DIR', dirname(__DIR__).'/');
 define('REACT_SRC_DIR', ROOT_DIR.'react/src/');
 define('REACT_LIB_DIR', ROOT_DIR.'react/lib/');
 define('SERVER_DIR', ROOT_DIR.'server/');
 define('GRAPHQL_DIR', SERVER_DIR.'types/');
-define(CONF_DIR, ROOT_DIR.'conf/');
-define(DEFAULT_DIR, CONF_DIR.'default/');
+define('CONF_DIR', ROOT_DIR.'conf/');
+define('DEFAULT_DIR', CONF_DIR.'default/');
 define('IS_INSTALLED', is_dir(REACT_SRC_DIR));
